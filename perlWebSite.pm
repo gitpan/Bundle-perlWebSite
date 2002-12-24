24237c3c009f9fc0e7dc781ab51360ae807867cb
Duncan McGreggor <oubiwann@myrealbox.com>
2002-12-24 00:36:21 +0000

    Import of OUBIWANN/Bundle-perlWebSite-0.02 from CPAN.
    gitpan-cpan-distribution: Bundle-perlWebSite
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         OUBIWANN/Bundle-perlWebSite-0.02.tar.gz
    gitpan-cpan-author:       OUBIWANN
    gitpan-cpan-maturity:     released

--- perlWebSite.pm
+++ perlWebSite.pm
@@ -1,6 +1,6 @@
 package Bundle::perlWebSite;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 1;
 
@@ -12,44 +12,44 @@ Bundle::perlWebSite - a bundle for perlWebSite
 
 =head1 SYNOPSIS
 
-  C<perl -MCPAN -e "install 'Bundle::perlWebSite'">
+ perl -MCPAN -e "install 'Bundle::perlWebSite'"
 
 =head1 CONTENTS
 
-CGI [ 2.89 ]
-CGI::Cache [ 1.40 ]
-CGI::Carp [ 1.24 ]
-CGI::Util [ 1.3 ]
-Cache::Cache [ 1.01 ]
-Digest::SHA1 [ 2.01 ]
-Error [ 0.15 ]
-File::Find::Rule [ 0.08 ]
-File::Temp [ 0.12 ]
-Number::Compare [ 0.01 ]
-Storable [ 2.06 ]
-Template [ 2.08 ]
-Template::Base [ 2.55 ]
-Template::Config [ 2.55 ]
-Template::Constants [ 2.54 ]
-Template::Context [ 2.69 ]
-Template::Directive [ 2.16 ]
-Template::Document [ 2.56 ]
-Template::Exception [ 2.51 ]
-Template::Filters [ 2.59 ]
-Template::Grammar [ 2.17 ]
-Template::Iterator [ 2.53 ]
-Template::Parser [ 2.66 ]
-Template::Plugins [ 2.58 ]
-Template::Provider [ 2.62 ]
-Template::Service [ 2.60 ]
-Template::Stash [ 2.68 ]
-Text::Glob [ 0.05 ]
-Text::WikiFormat [ 0.45 ]
-Time::Piece [ 1.08 ]
-URI::Escape [ 3.16 ]
-XML::Parser [ 2.31 ]
-XML::Parser::Expat [ 2.31 ]
-XML::Simple [ 1.05 ]
+ CGI [ 2.89 ]
+ CGI::Cache [ 1.40 ]
+ CGI::Carp [ 1.24 ]
+ CGI::Util [ 1.3 ]
+ Cache::Cache [ 1.01 ]
+ Digest::SHA1 [ 2.01 ]
+ Error [ 0.15 ]
+ File::Find::Rule [ 0.08 ]
+ File::Temp [ 0.12 ]
+ Number::Compare [ 0.01 ]
+ Storable [ 2.06 ]
+ Template [ 2.08 ]
+ Template::Base [ 2.55 ]
+ Template::Config [ 2.55 ]
+ Template::Constants [ 2.54 ]
+ Template::Context [ 2.69 ]
+ Template::Directive [ 2.16 ]
+ Template::Document [ 2.56 ]
+ Template::Exception [ 2.51 ]
+ Template::Filters [ 2.59 ]
+ Template::Grammar [ 2.17 ]
+ Template::Iterator [ 2.53 ]
+ Template::Parser [ 2.66 ]
+ Template::Plugins [ 2.58 ]
+ Template::Provider [ 2.62 ]
+ Template::Service [ 2.60 ]
+ Template::Stash [ 2.68 ]
+ Text::Glob [ 0.05 ]
+ Text::WikiFormat [ 0.45 ]
+ Time::Piece [ 1.08 ]
+ URI::Escape [ 3.16 ]
+ XML::Parser [ 2.31 ]
+ XML::Parser::Expat [ 2.31 ]
+ XML::Simple [ 1.05 ]
 
 
 =head1 DESCRIPTION
@@ -60,7 +60,7 @@ procedure:
 
  h2xs -AXcfn Bundle::perWebSite
 
-Edited the .pm file appropriately. Then, from a bash prompt:
+and then editing the .pm file appropriately. Then, from a bash prompt:
 
  perl -d:Modlist -MDevel::Modlist=nocore index.cgi 2>&1|egrep '^[A-Z].*[0-9]$'|awk '{print $1 " [ " $2 " ]"}' >> Bundle::perlWebSite.pm
 
